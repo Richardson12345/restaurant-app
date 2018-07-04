93ec65210370c475e24a4ded98ef3264e56e39c9
Richardson12345 <chuddywarrior@gmail.com>
2018-07-05 05:31:35 +0700

    msih banyak error

--- app.js
+++ app.js
@@ -21,6 +21,50 @@ app.get('/',(req,res)=>{
     res.render('login',{err:null})
 })
 
+app.get('/newAccount',(req,res) =>{
+    res.render('newAccount')
+})
+
+app.post('/newAccount', (req,res)=>{
+    let admin = req.body.isAdmin;
+    if(admin == "true"){
+        let obj = {
+            firstName : req.body.firstName,
+            lastName : req.body.lastName,
+            address : req.body.address,
+            city : req.body.city,
+            username : req.body.username,
+            password : req.body.password,
+            voucher : req.body.voucher,
+            isAdmin : 1
+        }
+        model.User.create(obj)
+        .then(()=>{
+            res.redirect('/')
+        })
+        .catch((err)=>{
+            res.json(err)
+        })
+    }else{
+        let obj = {
+            firstName : req.body.firstName,
+            lastName : req.body.lastName,
+            address : req.body.address,
+            city : req.body.city,
+            username : req.body.username,
+            password : req.body.password,
+            voucher : req.body.voucher
+        }
+        model.User.create(obj)
+        .then(()=>{
+            res.redirect('/')
+        })
+        .catch((err)=>{
+            res.json(err)
+        })
+    }
+})  
+
 app.get('/login',(req,res)=>{
     // res.send("home")
     res.render('login',{err:null})
@@ -59,6 +103,23 @@ app.post('/login',(req,res)=>{
     })
 })
 
+app.get('/admin/order',(req,res)=>{
+    res.render("addMenu")
+})
+
+app.post('/admin/add-menu',(req,res)=>{
+    model.Menu.create(
+        {
+            name: req.body.name,
+            price: req.body.price
+        }
+    ).then(()=>{
+        res.render('addedMenu', {
+            name : req.body.name
+        })
+    })
+})
+
 app.get('/home/admin',(req,res)=>{
     res.render('home-admin')
 })
@@ -67,7 +128,19 @@ app.use('/',admin)
 
 
 app.get('/home/user',(req,res)=>{
-    res.send("tampilan home dari user")
+    res.render('homeUser.ejs')
+})
+
+app.get('/menuList', (req,res)=>{
+    model.Menu.findAll({})
+    .then((itemArr)=>{
+        res.render('menu', {
+            itemArr
+        })
+    })
+    .catch((err)=>{
+        res.json(err)        
+    })
 })
 
 app.use('/user',user)
